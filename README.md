1a9180d58e3f88ab313bece3590272406452be03
Harish Prabhu <111897267+Harishprabhu30@users.noreply.github.com>
2024-06-23 10:12:18 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ conda create -n venv(virtual enviro. name) python=python_version
 
 Step-2: Activate the Virtual Environment using this code:
 conda activate venv(virtual Enviro. name)
+** python version for installing the requirements.txt is 3.10. 
+NOTE: IF YOU INSTALL REQUIREMTS.TXT ON PYTHON 3.11, IT WILL THROW ERROR WHILE CREATING METADATA DURING DOWNLOADING SCIKIT-LEARN AND SCIPY.
 
 Step-3: Install the requirements.txt file using this code:
 pip install -r requirements.txt
--- basic_network.py
+++ basic_network.py
@@ -1,8 +1,16 @@
-Class Network(object):
+import numpy as np
+import random 
+import time
 
-def __init__(self, sizes):
-    self.num_layers = len(sizes)
-    self.sizes = sizes
-    self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-    self.weights = [np.random.radn(y, x) 
+Class Network(object):
+    
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.radn(y, x) 
                     for x, y in zip(sizes[:-1], sizes[1:])]
+        
+## Creating Sigmoid Activation Function
+def sigmoid(z):
+    return 1.0/(1.0 + np.exp(-z))
